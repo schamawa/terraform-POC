dc811810e045d2b79812781c48f11accc9ec704f
subodh <subodhchamawar@gmail.com>
2022-08-17 10:35:56 +0000

    commited one code

---
+++ aws_instance.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_instance" "dummy" {
+  ami           = "ami-076e3a557efe1aa9c"
+  instance_type = "t2.micro"
+
+
+tags {
+ name       = "instance"
+ envirnment = "test"
+
+}
+} 

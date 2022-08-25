793578a58d1c43976b66be4c4006a080631a93b5
root <root@ip-172-31-0-126.ap-south-1.compute.internal>
2022-08-25 07:25:09 +0000

    created new

--- vpc_aws.tf
+++ vpc_aws.tf
@@ -20,12 +20,39 @@ resource "aws_subnet" "vpc_subnet" {
 }
 }
 
+resource "aws_security_group" "test" {
+  name = "test"
+  vpc_id = aws_vpc.test_vpc.id
+  ingress {
+  from_port = "22"
+  to_port = "22"
+  protocol = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
+egress {
+    from_port = 0
+    protocol = "-1"
+    to_port = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 resource "aws_instance" "test" {
    ami = "ami-076e3a557efe1aa9c"
    subnet_id = aws_subnet.vpc_subnet.id
    instance_type = "t2.micro"
+   vpc_security_group_ids = [aws_security_group.test.id]
 
    tags = {
     name = "prod"
 }
+}
+
+output "aws_instance_private_ip" {
+  value = aws_instance.test.private_ip
+}
+
+output "aws_security_group_name" {
+  value = aws_security_group.test.id
+
 }
